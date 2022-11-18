bace678e43ed7bc21655afd4f28815e7f23c2987
kavis777 <wwrd0129@gmail.com>
2022-11-18 16:54:39 +0900

    VS Codeでキーを連打できるように設定

--- config_setup.sh
+++ config_setup.sh
@@ -7,6 +7,9 @@ killall Finder
 # dotfilesをホームディレクトリにクローン
 git clone https://github.com/kavis777/dotfiles.git ~/dotfiles
 
+# VS Codeでキーを連打できるように設定
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+
 # 各設定ファイルのシンボリックリンクをホームディレクトリに作成
 ln -s ~/dotfiles/.gitconfig ~
 ln -s ~/dotfiles/.gitignore_global ~
