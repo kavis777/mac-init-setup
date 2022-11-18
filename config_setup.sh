5aa71dfcd57ed23507b996f2f88402786ffef37b
kavis777 <wwrd0129@gmail.com>
2022-11-18 16:10:50 +0900

    docs: シンボリックを生成する処理を追加

--- config_setup.sh
+++ config_setup.sh
@@ -11,3 +11,4 @@ git clone https://github.com/kavis777/dotfiles.git ~/dotfiles
 ln -s ~/dotfiles/.gitconfig ~
 ln -s ~/dotfiles/.gitignore_global ~
 ln -s ~/dotfiles/zsh/.zshrc ~
+ln -s ~/dotfiles/tmux/.tmux.conf ~
