5fa847c1cd7f32a00686dcabe729c9f188fdced2
kavis777 <wwrd0129@gmail.com>
2022-12-04 11:32:03 +0900

    brew install処理を更新

--- install_brew_app.sh
+++ install_brew_app.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 # Homebrewでアプリをインストール
-for app in `cat app_list/brew.txt`; do
-  if ! type ${app} > /dev/null 2>&1; then
-    brew install ${app}
-  else
+for app in $(cat app_list/brew.txt);
+do
+  brew list "${app}" > /dev/null 2>&1
+  if test $? -eq 0;
+  then
     echo "Installed ${app}"
+  else
+    brew install "${app}"
   fi
 done
